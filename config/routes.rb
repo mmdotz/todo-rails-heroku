0643d0c51faeaf661bc90edb50edfaca70ab7f65
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-09-21 18:17:20 -0500

    added background image to body

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   put    '/tasks/:id', to: 'tasks#update'
   delete '/tasks/:id', to: 'tasks#destroy'
 
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
